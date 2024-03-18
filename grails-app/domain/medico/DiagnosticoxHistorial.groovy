c57a5bee92554a13786b5c6c7f9fcc357b9931a5
Fabricio <fegrijalva2501@hotmail.com>
2024-03-18 14:31:42 -0500

    ctdg

---
+++ grails-app/domain/medico/DiagnosticoxHistorial.groovy
@@ -0,0 +1,27 @@
+package medico
+
+class DiagnosticoxHistorial {
+
+    Diagnostico diagnostico
+    Historial historial
+    String descripcion
+
+    static mapping = {
+        table 'ctdg'
+        cache usage: 'read-write', include: 'non-lazy'
+        id column: 'ctdg__id'
+        id generator: 'identity'
+        version false
+        columns {
+            diagnostico column: 'diag__id'
+            historial column: 'hscl__id'
+            descripcion column: 'ctdgdscr'
+        }
+    }
+
+    static constraints = {
+        diagnostico(blank: false, nullable: false)
+        historial(blank: false, nullable: false)
+        descripcion(size: 0..255, blank: false, nullable: false)
+    }
+}
